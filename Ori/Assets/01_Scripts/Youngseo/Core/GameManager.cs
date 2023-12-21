8ede1d4271a64cd6f3a93d893c1270684787de38
luca3 <dudsj9782@gmail.com>
2023-12-21 20:27:19 +0900

    머티리얼 왜 이래

--- Ori/Assets/01_Scripts/Youngseo/Core/GameManager.cs
+++ Ori/Assets/01_Scripts/Youngseo/Core/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameManager Instance;
+    
+    [SerializeField]
+    private PoolingListSO _poolListSO;
 
     private void Awake()
     {
@@ -12,13 +15,12 @@ public class GameManager : MonoBehaviour
         
     }
 
-    private void CreateCameraManager()
-    {
-        
-    }
-
     private void CreatePoolManager()
     {
-        
+        PoolManager.Instance = new PoolManager(transform);
+        foreach (var pair in _poolListSO.Pairs)
+        {
+            PoolManager.Instance.CreatePool(pair.Prefab, pair.Count);
+        }
     }
 }
