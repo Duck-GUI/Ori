39a9d9dc7d6a5fb26f36423fdf3f2fb08deae0be
qls051 <qls050100@naver.com>
2023-12-26 12:43:13 +0900

    ui toolkit, sound
    사운드 찾고 넣고 start scene 수정

--- Ori/Assets/01_Scripts/Yebin/MenuControllor.cs
+++ Ori/Assets/01_Scripts/Yebin/MenuControllor.cs
@@ -10,15 +10,23 @@ public class MenuControllor : MonoBehaviour
     private UIDocument doc;
 
     private Button playButton;
+
+    [Header("Setting Button")]
+    [SerializeField] private VisualTreeAsset settingButtonsTemplate;
+    private VisualElement settingButtons;
     private Button settingButton;
+    private VisualElement buttonsWrapper;
+
     private Button exitButton;
 
     [Header("Mute Button")]
     [SerializeField] private Sprite mutedSprite;
     [SerializeField] private Sprite unmutedSprite;
     private bool muted;
-
     private Button muteButton;
+
+    [SerializeField] private AudioClip buttonClickSound;
+    public AudioSource audioSource;
 
     private void Awake()
     {
@@ -27,28 +35,57 @@ public class MenuControllor : MonoBehaviour
         playButton = doc.rootVisualElement.Q<Button>("PlayButton");
         playButton.clicked += PlayButtoOnClicked; // () => { Dosomething();}
 
-        playButton = doc.rootVisualElement.Q<Button>("SettingButton");
+        buttonsWrapper = doc.rootVisualElement.Q<VisualElement>("Buttons");
 
+        settingButton = doc.rootVisualElement.Q<Button>("SettingButton");
+        settingButton.clicked += SettingButtonOnClick;
 
-        playButton = doc.rootVisualElement.Q<Button>("ExitButton");
+        settingButtons = settingButtonsTemplate.CloneTree();
+        var backButton = settingButtons.Q<Button>("BackButton");
+        backButton.clicked += BackButtonOnClicked;  
+
+        exitButton = doc.rootVisualElement.Q<Button>("ExitButton");
         exitButton.clicked += ExitButtOnClicked;
 
-        playButton = doc.rootVisualElement.Q<Button>("MuteButton");
+        muteButton = doc.rootVisualElement.Q<Button>("MuteButton");
         muteButton.clicked += MuteButtonOnClicked;
     }
 
     private void PlayButtoOnClicked()
     {
         SceneManager.LoadScene("Test"); // Scene 이름 수정
+        audioSource.PlayOneShot(buttonClickSound);
+    }
+
+    private void SettingButtonOnClick()
+    {
+        buttonsWrapper.Clear();
+        buttonsWrapper.Add(settingButtons);
+
+        audioSource.PlayOneShot(buttonClickSound);
+    }
+
+    private void BackButtonOnClicked()
+    {
+        buttonsWrapper.Clear();
+        buttonsWrapper.Add(playButton);
+        buttonsWrapper.Add(settingButton);
+        buttonsWrapper.Add(exitButton);
+
+        audioSource.PlayOneShot(buttonClickSound);
     }
 
     private void ExitButtOnClicked()
     {
         Application.Quit();
+
+        audioSource.PlayOneShot(buttonClickSound);
     }
 
     private void MuteButtonOnClicked()
     {
+        audioSource.PlayOneShot(buttonClickSound);
+        
         muted = !muted;
         var bg = muteButton.style.backgroundImage;
         bg.value = Background.FromSprite(muted ? mutedSprite : unmutedSprite);
