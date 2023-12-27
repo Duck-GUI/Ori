8372dc5f78f36e60836fb01c30fddbce73a9c7e8
kainel54 <kainel0504@gmail.com>
2023-12-28 08:33:28 +0900

    일단 다 합침

--- Ori/Assets/01_Scripts/Yebin/LoginSceneBtnClick.cs
+++ Ori/Assets/01_Scripts/Yebin/LoginSceneBtnClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoginSceneBtnClick : MonoBehaviour
 {
@@ -10,5 +11,13 @@ public class LoginSceneBtnClick : MonoBehaviour
     public void LoginBtnClick()
     {
         audioSource.PlayOneShot(buttonClickSound);
+    }
+
+    public void Entrance()
+    {
+        audioSource.PlayOneShot(buttonClickSound);
+        int index = Random.Range(0, 2);
+        string[] maps = { "Map_1", "Map_4", "Map_5" };
+        SceneManager.LoadScene(maps[index]);
     }
 }
